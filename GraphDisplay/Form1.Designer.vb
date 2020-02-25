8540e8798dbcd6832055686a22c140a5cd8c904e
yosshii50 <kasaubunts@gmail.com>
2020-02-25 10:23:12 +0900

    Initial commit

---
+++ GraphDisplay/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(86, 25)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(596, 335)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(429, 366)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(171, 72)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(802, 467)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ GraphDisplay/Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Static PubBITMAP As Bitmap
+        If PubBITMAP Is Nothing Then
+            PubBITMAP = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        End If
+
+        Call DrawGp(PubBITMAP)
+
+        PictureBox1.Image = PubBITMAP
+
+    End Sub
+
+    Private Sub DrawGp(ByVal GetBITMAP As System.Drawing.Bitmap)
+
+        Dim ds As DrawSet = DrawInit(GetBITMAP)
+
+        For EleX As Integer = GetBITMAP.Width * -1 To GetBITMAP.Width
+
+            Dim NowPosX As Integer
+            Dim NowPosY As Integer
+
+            NowPosX = EleX
+
+            'ここが一次関数
+            NowPosY = NowPosX * 10 + 100
+
+            DrawXY(ds, NowPosX, NowPosY)
+        Next
+
+        'リソースを解放する
+        ds.g.Dispose()
+
+    End Sub
+
+    Structure DrawSet
+        Dim g As System.Drawing.Graphics
+        Dim XMax As Integer
+        Dim YMax As Integer
+        Dim XPos As Integer
+        Dim YPos As Integer
+        Dim LastPosX As Integer
+        Dim LastPosY As Integer
+    End Structure
+    Private Function DrawInit(ByVal GetBITMAP As System.Drawing.Bitmap) As DrawSet
+
+        Dim ds As DrawSet
+
+        ds.g = System.Drawing.Graphics.FromImage(GetBITMAP)
+
+        ds.XMax = GetBITMAP.Width
+        ds.YMax = GetBITMAP.Height
+        ds.XPos = GetBITMAP.Width / 2
+        ds.YPos = GetBITMAP.Height / 2
+        ds.LastPosX = -1
+        ds.LastPosY = -1
+
+        ds.g.FillRectangle(Brushes.White, New Rectangle(0, 0, ds.XMax, ds.YMax))
+
+        For WrkStep As Integer = 0 To ds.YPos Step 20
+
+            Dim WrkPens As System.Drawing.Pen
+            If WrkStep = 0 Then
+                WrkPens = Pens.SeaGreen
+            Else
+                WrkPens = Pens.SkyBlue
+            End If
+
+            ds.g.DrawLine(WrkPens, 0, ds.YPos + WrkStep, ds.XMax, ds.YPos + WrkStep)
+            ds.g.DrawLine(WrkPens, 0, ds.YPos + WrkStep * -1, ds.XMax, ds.YPos + WrkStep * -1)
+        Next
+        For WrkStep As Integer = 0 To ds.XPos Step 20
+
+            Dim WrkPens As System.Drawing.Pen
+            If WrkStep = 0 Then
+                WrkPens = Pens.SeaGreen
+            Else
+                WrkPens = Pens.SkyBlue
+            End If
+
+            ds.g.DrawLine(WrkPens, ds.XPos + WrkStep, 0, ds.XPos + WrkStep, ds.YMax)
+            ds.g.DrawLine(WrkPens, ds.XPos + WrkStep * -1, 0, ds.XPos + WrkStep * -1, ds.YMax)
+
+        Next
+
+        Return ds
+    End Function
+    Private Sub DrawXY(ByRef ds As DrawSet, ByVal X As Integer, ByVal Y As Integer)
+
+        Y = Y * -1
+
+        If ds.LastPosX = -1 Then
+            ds.LastPosX = X
+            ds.LastPosY = Y
+        End If
+
+        'ds.g.DrawRectangle(Pens.Black, ds.XPos + X, ds.YPos + Y, 1, 1)
+        ds.g.DrawLine(Pens.Black, ds.XPos + ds.LastPosX, ds.YPos + ds.LastPosY, ds.XPos + X, ds.YPos + Y)
+
+        ds.LastPosX = X
+        ds.LastPosY = Y
+
+    End Sub
+
+End Class
